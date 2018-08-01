b15258d32b7f13b49ca05cd62d8210dd2008974d
Nolan Magee <nolanmagee@gmail.com>
2018-08-01 14:19:18 -0700

    Finished MessagesList.js

--- src/components/MessagesList.js
+++ src/components/MessagesList.js
@@ -3,5 +3,26 @@ import PropTypes from 'prop-types'
 import Message from './message'
 
 const MessagesList = ({messages}) => (
-  <section id ='m'
+  <section id ='messages-list'>
+    <ul>
+      {messages.map(message=>(
+        <Message
+          key={message.id}
+          {...message}
+        />
+      ))}
+    </ul>
+  </section>
 )
+
+MessagesList.propTypes = {
+  messages: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      message: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired
+}
+
+export default MessagesList
