5ed6fd29c51a56e85dd7be429729861fd30628a4
Nolan Magee <nolanmagee@gmail.com>
2018-08-11 17:27:29 -0700

    Added websocket server/client

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import {Provider} from 'react-redux';
 import chat from './reducers/index';
 import {createStore} from 'redux';
 
-
+//The default test fails with Redux and I haven't bothered to fix it
 it('renders without crashing', () => {
   expect(1+1).toEqual(2);
 });
--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import createSagaMiddleware from 'redux-saga'
 
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import chat from './reducers/index';
-import {addUser} from './actions';
+import reducers from './reducers/index';
+import handleNewMessage from './sagas'
+import setupSocket from './sockets'
+import username from './utils/name'
 
-const store = createStore(chat)
+const sagaMiddleware = createSagaMiddleware()
 
-store.dispatch(addUser('Me'))
+const store = createStore(
+  reducers,
+  applyMiddleware(sagaMiddleware)
+)
+
+const socket = setupSocket(store.dispatch, username)
+
+sagaMiddleware.run(handleNewMessage, {socket, username})
 
 ReactDOM.render(
   <Provider store={store}>
