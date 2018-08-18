57d343b53664f55a473cd306c538be04e7b27f1a
Nolan Magee <nolanmagee@gmail.com>
2018-08-18 11:11:41 -0700

    edited npm start

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.get('/', (req, res)=>{
   res.sendFile(path.join(__dirname, '../build/index.html'))
 })
 
-const port = process.env.PORT || 8989;
+const port = process.env.PORT || 5000;
 console.log("PORT IS: ", port)
 
 
